9e3d00d0621fcde97723379fb447dfbbb3d6a917
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-05-18 22:28:47 -0300

    feat: adicionando graphql no front

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,11 +1,26 @@
 import {getAccessToken, useUser, withPageAuthRequired} from '@auth0/nextjs-auth0'
+import { useQuery,gql } from '@apollo/client'
+import { withApollo } from '../../lib/withApollo'
 
-export default function Home(){
+const PRODUCTS_QUERY = gql`
+ query GetProducts {
+   products {
+     id
+     title
+   }
+ }
+`
+
+ function Home(){
   const {user} = useUser()
+  const {data, loading, error} = useQuery(PRODUCTS_QUERY)
 
   return (
     <div>
       <h1>Hello World</h1>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         {JSON.stringify(user,null,2)}
       </pre>
@@ -18,7 +33,11 @@ export const getServerSideProps = withPageAuthRequired({
     console.log(getAccessToken(req,res))
     
     return{
-      props:{}
+      props:{
+        apolloState
+      }
     }
   }
 })
+
+export default withApollo(Home)
